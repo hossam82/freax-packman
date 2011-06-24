a63298f7a8893f88530e4a4d7eaf658c01d54334
julius8774@gmail.com <julius8774@gmail.com@c836fb9d-5e9e-7909-83f3-af3105ea4df4>
2011-06-24 06:19:35 +0000

    Same as below (or above if you order in natural manner)
    git-svn-id: https://freax-packman.googlecode.com/svn/trunk@45 c836fb9d-5e9e-7909-83f3-af3105ea4df4

--- fpm-parser/src/it/freax/fpm/parser/Compiler.java
+++ fpm-parser/src/it/freax/fpm/parser/Compiler.java
@@ -3,15 +3,15 @@
  */
 package it.freax.fpm.parser;
 
+import it.freax.fpm.core.util.Collections;
+import it.freax.fpm.core.util.Constants;
+import it.freax.fpm.core.util.Streams;
+import it.freax.fpm.core.util.Strings;
 import it.freax.fpm.parser.actions.CompilationAction;
 import it.freax.fpm.parser.edo.ErrorReport;
 import it.freax.fpm.parser.edo.TargetCode;
 
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
 import java.io.IOException;
-import java.util.Scanner;
 
 /**
  * @author kLeZ-hAcK
@@ -24,44 +24,14 @@ public class Compiler
 	public static void main(String[] args) throws IOException
 	{
 		CompilationAction compilationAction = new CompilationAction();
-		TargetCode targetCode = compilationAction.run(read(new File(args[0])));
-		for (ErrorReport report : targetCode.getErrorReports())
+		TargetCode targetCode = compilationAction.run(Streams.getOne(args[0]).read());
+		Collections<ErrorReport> reports = null;
+		reports = Collections.getOne(targetCode.getErrorReports());
+		System.out.println(Strings.getOne().getLines(reports.<String> castAll()));
+		if (reports.isNullOrEmpty())
 		{
-			System.out.println(report);
+			String path = Constants.getOne().getDirPrefix() + args[1];
+			Streams.getOne(path).write(targetCode.getOutputPayload(), true).setExecutable(true);
 		}
-		if (targetCode.getErrorReports().isEmpty())
-		{
-			String path = System.getProperty("user.dir") + args[1];
-			write(path, targetCode.getOutputPayload()).setExecutable(true);
-		}
-	}
-
-	public static File write(String path, byte[] toWrite) throws IOException
-	{
-		File file = new File(path);
-		if (file.exists() || file.createNewFile())
-		{
-			FileOutputStream fileOutputStream = new FileOutputStream(file);
-			fileOutputStream.write(toWrite);
-			fileOutputStream.close();
-		}
-		else
-		{
-			throw new IOException("File not exists and cannot be created, aborting");
-		}
-		return file;
-	}
-
-	public static String read(File file) throws FileNotFoundException
-	{
-		StringBuilder ret = new StringBuilder();
-		Scanner scanner = new Scanner(file);
-		while (scanner.hasNext())
-		{
-			ret.append(scanner.nextLine());
-			ret.append(System.getProperty("line.separator"));
-		}
-		scanner.close();
-		return ret.toString();
 	}
 }
