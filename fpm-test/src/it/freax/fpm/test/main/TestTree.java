44989b1dcab728abcdc14a1984fac11a1be84662
julius8774@gmail.com <julius8774@gmail.com@c836fb9d-5e9e-7909-83f3-af3105ea4df4>
2011-12-12 06:44:04 +0000

    Expression evaluator implementation start...
    git-svn-id: https://freax-packman.googlecode.com/svn/trunk@73 c836fb9d-5e9e-7909-83f3-af3105ea4df4

---
+++ fpm-test/src/it/freax/fpm/test/main/TestTree.java
@@ -0,0 +1,22 @@
+/**
+ * 
+ */
+package it.freax.fpm.test.main;
+
+import it.freax.fpm.util.Tree;
+
+/**
+ * @author kLeZ-hAcK
+ * 
+ */
+public class TestTree
+{
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args)
+	{
+		Tree<String> root = new Tree<String>("");
+	}
+}
